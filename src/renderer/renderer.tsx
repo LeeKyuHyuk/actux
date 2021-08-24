981d381c4f1a116d4029a74d27590b3a88c57f0d
KyuHyuk Lee <lee@kyuhyuk.kr>
2021-08-24 21:31:26 +0900

    Create 'Actus' Project

---
+++ src/renderer/renderer.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div className="app">
+    <h1>Hello, Actux!</h1>
+  </div>,
+  document.getElementById('root'),
+);
